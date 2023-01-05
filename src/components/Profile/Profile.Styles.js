5b1eb96732537d28a4c90bcde6e89e7ed84139e5
andriideikalo <wlada801@gmail.com>
2023-01-05 12:28:33 +0200

    fiz

--- src/components/Profile/Profile.Styles.js
+++ src/components/Profile/Profile.Styles.js
@@ -22,6 +22,66 @@ export const ProfStyles = styled.div`
   ul {
     padding: 0;
   }
+  .description {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  .avatar {
+    display: block;
+    width: 300px;
+    border-radius: 50%;
+    margin-bottom: 30px;
+  }
+  .name {
+    font-weight: 800;
+    margin-bottom: 10px;
+  }
+
+  .tag {
+    font-weight: 800;
+    color: blue;
+    cursor: pointer;
+    margin-bottom: 10px;
+  }
+
+  .location {
+    color: grey;
+  }
+  p {
+    margin: 0;
+  }
+  .stats {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .stats > li {
+    padding: 10px;
+    width: 100px;
+    border: 1px solid #e7ecf2;
+    background-color: #f3f6f9;
+  }
+
+  .label {
+    display: block;
+    font-size: 18px;
+    text-align: center;
+    padding-bottom: 10px;
+    cursor: pointer;
+    color: grey;
+  }
+  .quantity {
+    display: block;
+    font-weight: 800;
+    text-align: center;
+    cursor: pointer;
+  }
 `;
 export const DescriptionStyles = styled.div`
   .description {
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,24 +7,24 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
     <ProfStyles>
       <div className="profile">
         <div className="description">
-          <img src={users.avatar} alt="User avatar" className="avatar" />
-          <p className="name">{users.username}</p>
-          <p className="tag">@{users.tag}</p>
-          <p className="location">{users.location}</p>
+          <img src={avatar} alt="User avatar" className="avatar" />
+          <p className="name">{username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
         </div>
 
         <ul className="stats">
           <li>
             <span className="label">Followers </span>
-            <span className="quantity">{users.stats.followers}</span>
+            <span className="quantity">{stats.followers}</span>
           </li>
           <li>
             <span className="label">Views </span>
-            <span className="quantity">{users.stats.views}</span>
+            <span className="quantity">{stats.views}</span>
           </li>
           <li>
             <span className="label">Likes </span>
-            <span className="quantity">{users.stats.likes}</span>
+            <span className="quantity">{stats.likes}</span>
           </li>
         </ul>
       </div>
